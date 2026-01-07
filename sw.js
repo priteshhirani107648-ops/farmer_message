9b75a6eb29cf04c787fa97b8e2f0c3e289682250
priteshhirani107648-ops <pritesh.hirani107648@gmail.com>
2026-01-07 20:51:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 const CACHE_NAME = "nilkanth-cache-v1";
-
 const FILES_TO_CACHE = [
   "./",
   "./index.html"
@@ -7,16 +6,44 @@ const FILES_TO_CACHE = [
 
 // Install
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
 // Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
+});
+
+// 🔥 MANUAL UPDATE HANDLER
+self.addEventListener("message", event => {
+  if (event.data === "FORCE_UPDATE") {
+    caches.delete(CACHE_NAME).then(() => {
+      self.skipWaiting();
+    });
+  }
 });
