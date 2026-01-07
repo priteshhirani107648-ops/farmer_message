d2241d3b21e3bbcfe792636109677da00dcb84b8
priteshhirani107648-ops <pritesh.hirani107648@gmail.com>
2026-01-07 20:45:14 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "nilkanth-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
